82d18b7d9ff7b33e293294d61d8f80bcf3cd8ab2
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-09 23:58:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(filePath)
   library = {"get_emoticon" => {}, "get_meaning" => {}}
   temp.each do |k, v| 
     library["get_emoticon"][v[i]] = k
+    library["get_meaning"][v[i]] = k
    # library["get_emoticon"] += v
     #library["get_meaning"] += k
   end
