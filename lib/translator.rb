6fe47324a02bf5f73f256cbc55a1448c04815e3a
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-10 00:05:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_japanese_emoticon(filePath, emoticon)
   # code goes here
-  library = loadLibrary(~\lib\emoticons.yml)
+  library = load_Library(filePath)
   return library["get_emoticon"][emoticon]
 end
 
